2770bbfb05988a83281eb8fe5292ade07d03ed94
tomy07417 <tamundarain@fi.uba.ar>
2022-05-29 23:02:23 +0000

    Implementacion de la app de gastos

---
+++ app_gastos/gastos.js
@@ -0,0 +1,39 @@
+var montoTotal = 0;
+var participantes = 0;
+
+function crearElemento(nombre, monto){
+
+    var contribuyentes = document.getElementById("contribuyentes");
+    var pEjemplo = document.getElementById("p-ejemplo");
+    var p = pEjemplo.cloneNode(true);
+
+    p.innerHTML = nombre + ": " + "$" + monto;
+
+    p.style.setProperty("padding", "none");
+
+    contribuyentes.appendChild(p);
+}
+
+function actualizarResumen(){
+    const montoFinal = document.getElementById("monto-total");
+    const montoDividido = document.getElementById("monto-dividido");
+
+    montoFinal.textContent = "TOTAL: $" + montoTotal;
+    montoDividido.textContent = "A cada uno le toca aportar: $" + (montoTotal / participantes);
+}
+
+function agregar(){
+
+    const nombre = document.getElementById("nombre");
+    const monto = document.getElementById("monto");
+
+    crearElemento(nombre.value, monto.value);
+
+    montoTotal += parseFloat(monto.value);
+    participantes++;
+
+    actualizarResumen();
+
+    nombre.value = "";
+    monto.value = "";
+}
