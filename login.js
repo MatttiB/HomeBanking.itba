5830c0da16c16ad503199b5f13e1a680e9b74620
Juan cruz Vittori <juancruz.vittori@gmail.com>
2022-06-15 14:44:11 -0300

    Rediseño del login y formulario registro

--- login.js
+++ login.js
@@ -1,37 +1,56 @@
+const container = document.getElementById('formulario')
+    ocultar = document.querySelectorAll('.showHidePw'),
+    campos = document.querySelectorAll('.password');
 
-// Funcion que captura los datos del form
+// CODIGO PARA OCULTAR
 
-function capturar(){
+ocultar.forEach(eyeIcon =>{
+    eyeIcon.addEventListener('click', ()=> {
+        campos.forEach (campos =>{
+            if(campos.type=== 'password'){
+                campos.type = 'text' ;
+            }else{
+                campos.type = 'password';
+           
+            }
+        })
+    })
 
-    function user(usuario,contraseña){
-        this.usuario=usuario;
-        this.password=password;
-    }
-    var nombyape=document.getElementById ('usuario').value;
-    var pass=document.getElementById ('password').value;
+})
+
+// // FUNCION ANTERIOR QUE CAPTURA LOS DATOS DEL FORM
+
+// function capturar(){
+
+//     function user(usuario,contraseña){
+//         this.usuario=usuario;
+//         this.password=password;
+//     }
+//     var nombyape=document.getElementById ('usuario').value;
+//     var pass=document.getElementById ('password').value;
     
-    nuevoUsuario= new user(nombyape,pass);
-    console.log(nuevoUsuario)
+//     nuevoUsuario= new user(nombyape,pass);
+//     console.log(nuevoUsuario)
 
-    if (nombyape=='') {
-        alert('El usuario es obligatorio')
-    }
+//     if (nombyape=='') {
+//         alert('El usuario es obligatorio')
+//     }
     
-    if (pass=='') {
-        alert('La contraseña es obligatoria')
-    }
+//     if (pass=='') {
+//         alert('La contraseña es obligatoria')
+//     }
 
-    console.log('La Usuario es:' + nombyape);
-    console.log('La contraseña es:' + pass);
-    agregar();
-}
+//     console.log('La Usuario es:' + nombyape);
+//     console.log('La contraseña es:' + pass);
+//     agregar();
+// }
 
 
-//array para guardar los nuevos usuarios
+// //array para guardar los nuevos usuarios
 
-var baseDatos = [];
-function agregar (){
-    baseDatos.push(nuevoUsuario);
-    console.log (baseDatos)
-}
+// var baseDatos = [];
+// function agregar (){
+//     baseDatos.push(nuevoUsuario);
+//     console.log (baseDatos)
+// }
 
