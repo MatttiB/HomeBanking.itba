a977d3ddb2f094af556f655344b364e9fb66fc37
tomy07417 <tamundarain@fi.uba.ar>
2022-06-27 10:47:05 +0000

    arreglos en app gastos

--- app_gastos/gastos.js
+++ app_gastos/gastos.js
@@ -1,102 +1,99 @@
-var montoTotal = 0;
-var participantes = 0;
-
-
-function crearElemento(nombre, monto){
-
-    var contribuyentes = document.getElementById("contribuyentes");
-    var pEjemplo = document.getElementById("p-ejemplo");
-    var p = pEjemplo.cloneNode(true);
+function armarNombre(cadena){
+    let armadoNombre = "";
     
-    p.onclick=quitar;
+    for(var j = 0; cadena[j] != ":" ; j++){
+        armadoNombre += cadena[j];
+    }
 
-    p.innerHTML = nombre + ": " + "$" + monto;
-
-    p.style.setProperty("padding", "none");
-
-    contribuyentes.appendChild(p);
+    return armadoNombre;
 }
 
-function actualizarResumen(){
-    const montoFinal = document.getElementById("monto-total");
-    const montoDividido = document.getElementById("monto-dividido");
-
-    montoFinal.textContent = "TOTAL: $" + montoTotal;
-    montoDividido.textContent = "A cada uno le toca aportar: $" + (montoTotal / participantes);
-
-    quitar();
-
-}
-
-function setErrorFor(input, message) {
-const formControl = input.parentElement;
-const small = formControl.querySelector('small');
-formControl.className = 'form-control error';
-small.innerText = message;
-
-}
-
-function setSuccessFor(input) {
-const formControl = input.parentElement;
-formControl.className = 'form-control success';
-}
-
-function agregar(){
-
-    const nombre = document.getElementById("nombre");
-    const monto = document.getElementById("monto");
-    var nombreAgregado = nombre.value;
-    var montoAgregado= monto.value;
-
-
-    if(nombreAgregado===''&&montoAgregado===''){
-        nombre.setAttribute('placeholder', 'Agregar un nombre válido');
-        monto.setAttribute('placeholder', 'Agregar un monto válido');
-        setErrorFor(nombre, 'Debe completar este campo');
-        setErrorFor(monto, 'Debe completar este campo')
-        return false;}
-        
-
-    if(nombreAgregado===''){
-        nombre.setAttribute('placeholder', 'Agregar un nombre válido');
-        setErrorFor(nombre, 'Debe completar este campo');
-        return false;
+function buscarNombre(nombre, lista){
+    let index;
+    for(var i = 0; i < lista.length; i++){
+        if(lista[i][0] == nombre){
+            index = i;
         }
-
-    if(montoAgregado===''){
-        monto.setAttribute('placeholder', 'Agregar un monto válido');
-        setErrorFor(monto, 'Debe completar este campo')
-        return false;
-        
-        
     }
-    
-    crearElemento(nombre.value, monto.value);
-
-    montoTotal += parseFloat(monto.value);
-    participantes++;
-
-    actualizarResumen();
-
-    nombre.value =  "";
-    monto.value = "";
-    nombre.setAttribute('placeholder', 'Nombre');
-    monto.setAttribute('placeholder', 'Monto');
-    setSuccessFor(monto);
-    setSuccessFor(nombre);
+    return index
 }
-    
-function quitar () {
-    document.getElementById('contribuyentes').removeChild(this);
 
-    montoTotal -= parseFloat (monto.value);
-   
-    if (montoTotal<=0) {
-        contribuyentes.setAttribute('placeholder', 'A cada uno le toca aportar');
-    }
-    
-    actualizarResumen();
-    agregar();
+function actualizarResumen(montoTotal, participantes){
+    const total = document.getElementById("monto-total"),
+        montoParticipante = document.getElementById("monto-dividido");
 
+    total.textContent = "TOTAL: $" + montoTotal;
+    montoParticipante.textContent = "A cada uno le toca aportar: $" + (montoTotal/participantes);
 }
+
+(function(){
+    const nombre = document.getElementById("nombre"),
+        monto = document.getElementById("monto"),
+        boton = document.getElementById("boton");
+
+    let montoTotal = 0;
+    let participantes = 0;
+
+    let listaParticipantes = [];
+    
+    const agregarMonto = function(){
+        if (nombre.value == "" || isNaN(monto.value) || monto.value == ""){
+
+            if (nombre.value == ""){
+                nombre.value = "";
+                nombre.setAttribute("placeholder" , "Ingrese un nombre válido");
+                nombre.className = "form-control w-50 mb-2 border-danger";
+            } 
+            if(isNaN(monto.value) || monto.value == "") {
+                monto.value = "";
+                monto.setAttribute("placeholder" , "Ingrese un monto válido");
+                monto.className = "form-control w-50 mb-2 border-danger";  
+            }
+
+        }else {
+
+            const contribuyentes = document.getElementById("contribuyentes");
+
+            const p = document.createElement("p");
+            p.textContent = nombre.value + ": $" + monto.value;
+            p.className = "remove"
+
+            contribuyentes.appendChild(p);
+
+            listaParticipantes.push([nombre.value, parseFloat(monto.value)]);
+
+            montoTotal += parseFloat(monto.value);
+            participantes++;
+
+            actualizarResumen(montoTotal, participantes);
+
+            monto.className = "form-control w-50 mb-2";
+            nombre.className = "form-control w-50 mb-2";
+
+            monto.value = "";
+            monto.setAttribute("placeholder" , "Monto");
+
+            nombre.value = "";
+            nombre.setAttribute("placeholder" , "Nombre");
+
+            for(var i = 1; i < contribuyentes.children.length; i++){
+                contribuyentes.children[i].addEventListener("click", function(){
+                    let nombre = armarNombre(this.textContent);
+                    let index = buscarNombre(nombre, listaParticipantes);
+
+                    montoTotal -= listaParticipantes[index][1];
+                    participantes--;
+
+                    actualizarResumen(montoTotal, participantes);
+
+                    listaParticipantes.splice(index,1);
+
+                    this.parentNode.removeChild(this);
+                });
+            }
+        }
+    };
+
+    boton.addEventListener("click", agregarMonto);
+}())
 
